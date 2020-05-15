cb6c766bced0ce77441a5f9a2985d974cc9f03c9
Haider <shkhaider2015@gmail.com>
2020-05-16 02:52:50 +0500

    change mind

--- MyApp/admin/routes.py
+++ MyApp/admin/routes.py
@@ -43,6 +43,7 @@ def admin_order():
     objectList.reverse()
     return render_template("admin_order.html", objectList=objectList)
 
+
 @admin.route("/accept_process", methods=['POST'])
 def process_accept():
     data = None
@@ -55,6 +56,7 @@ def process_accept():
         print(order_id)
         db.child("Orders").child(order_id).update(pair)
         db.child("Users").child(user_id).child("Orders").child(order_id).update(pair)
+
         return json.dumps({'status':'OK'})
     print("Not a Post Request")
 
@@ -68,53 +70,50 @@ def user_profile(uid):
     
     return render_template("profile.html", profile=profile, orders=orders)
 
-@admin.route("/messaging")
-def chat():
-    data = db.child("Chat").get()
-    data = dict(data.val())
-    user_msg = dict()
-    for k,v in data.items():
-        if v['senderId'] not in user_msg:
-            user_msg[v['senderId']] = {k:v}
-        else:
-            user_msg[v['senderId']].update({k:v})
-    side_bar = list()
-    for k,v in user_msg.items():
-        for kk,vv in v.items():
-            side_bar.append({kk:vv})
-            break
-    side_bar.reverse()
-    first_person = side_bar[0]
-    for k,v in first_person.items():
-        first_person = v
-    first_person_data = db.child("Users").child(first_person['senderId']).child("Chat").get()
-    first_person_data = dict(first_person_data.val())
+def get_data(key):
+    print(key)
+    tmp = db.child("Users").child(key).child("Chat").get()
+    dicts = dict(tmp.val())
+    print(dicts)
+    return dicts
+
+
+@admin.route("/messaging/<string:key>", methods=['POST', 'GET'])
+def chat(key):
     
-    return render_template("chat_room.html", first_person_data=first_person_data, side_bar=side_bar, senderID=first_person['senderId'])
+    return render_template("chat_room.html")
 
 @admin.route("/send_message", methods=['POST'])
 def message_send():
     data = None
     msg = None
     if request.method == 'POST':
-        time = datetime.now().timestamp() * 1000
+        time = int(datetime.now().timestamp()) * 1000
         data = request.form['data']
-        msg = request.form['msg']
+        msg = str(request.form['msg'])
         senderid = request.form['senderid']
-        if data == 'admin' and msg != None:
+        image_path =  "https://firebasestorage.googleapis.com/v0/b/canteen-management-syste-e183d.appspot.com/o/Admin%2Fburger.png?alt=media&token=b6731782-c9a6-452a-8bb6-46a4ce8a8321"
+        if data == 'adminn' and msg != None:
             print("Admin Call")
-            pushData = { 
+            pushData = dict({ 
                 'senderId' : "17352015",
                 'senderName' : "Admin",
                 'senderEmail' : "admin@gmail.com",
-                'senderProfileUri' : str("https://firebasestorage.googleapis.com/v0/b/canteen-management-syste-e183d.appspot.com/o/Admin%2Fburger.png?alt=media&token=b6731782-c9a6-452a-8bb6-46a4ce8a8321"),
+                'senderProfileUri' : image_path,
                 'msgTime' : time,
-                'msg' : msg}
+                'msg' : msg
+                })
 
-            db.child("Users").child(senderid).child("Chat").child(time).push(pushData)
+            print(f"The Sender Id : {senderid} and time : {time} ")
+            db.child("Users").child(senderid).child("Chat").child(time).set(pushData)
+            return json.dumps({'status' : 'ok'})
             
+        elif data == "page":
+            return json.dumps(get_data(senderid))
         else:
             print("Another Call")
+            
         return json.dumps({'status' : 'ok'})
     print("Not a post Request")
+
 
